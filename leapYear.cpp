711a4c60987ecd7db57cea277d1c1513ccbc6dbd
 <surajsingh5092@gmail.com>
2021-02-06 07:00:13 +0530

    leap year

---
+++ leapYear.cpp
@@ -0,0 +1,15 @@
+// Program to check whether a year is leap or not
+#include <iostream>
+using namespace std;
+int main()
+{
+    int year;
+    cout << "Enter year: ";
+    cin >> year;
+
+    if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+        cout << year << " is a leap year." << endl;
+    else
+        cout << year << " is not a leap year." << endl;
+    return 0;
+}
