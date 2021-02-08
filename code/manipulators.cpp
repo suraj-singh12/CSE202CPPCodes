73153c0035ce9af50f022c2481fe7f81784efea2
 <surajsingh5092@gmail.com>
2021-02-08 15:12:24 +0530

    manipulators in c++

---
+++ code/manipulators.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string s1,s2;
+    cout << "Input two string : ";
+    getline(cin,s1);
+    getline(cin,s2);
+    cout << s1 << endl << s2 << endl;
+    return 0;
+}
