02925a246abc99987260771a8cba6575439f84eb
suraj-singh12 <surajsingh5092@gmail.com>
2021-04-08 11:31:56 +0530

    adding a Basic inheritance example

---
+++ code/After MTE/inheritance.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Animal{
+    public:
+     int leg = 2;
+     int eye = 2;
+     bool fly = true;
+};
+
+class Cat : public Animal{
+    public:
+     Cat(){
+         fly = false;
+     }
+};
+
+int  main()
+{
+    Cat c1; 
+    cout << "Legs: " << c1.leg << "\t\tEyes: " << c1.eye << "\t\tCan Fly? " << (c1.fly?"Yes":"No") << endl;
+    return 0;
+}
